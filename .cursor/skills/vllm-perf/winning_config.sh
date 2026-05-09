730f1ea45ebf4bf2708281feb50865b52544c4e7
Adam Pavlat <adam.pavlat>
2026-05-09 21:35:51 +0200

    chore: remove stale winning_config.sh

--- .cursor/skills/vllm-perf/winning_config.sh
+++
@@ -1,5 +0,0 @@
-export VLLM_PERF_FLAGS="--batch-size 16"
-export VLLM_PERF_FLAGS='--batch-size 32'
-export VLLM_PERF_FLAGS='--batch-size 64'
-export VLLM_PERF_FLAGS='--batch-size 64 --dtype float32'
-export VLLM_PERF_FLAGS='--batch-size 128'
